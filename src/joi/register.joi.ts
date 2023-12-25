42306d9bd76c60f4a3a8ed0f4d97b8a4e856bd39
raviddavda <139677733+raviddavda@users.noreply.github.com>
2023-12-25 17:30:26 +0200

    improved joi error messages

--- src/joi/register.joi.ts
+++ src/joi/register.joi.ts
@@ -20,13 +20,31 @@ const registerSchema = Joi.object<IUser>({
     url: Joi.string().min(12).max(200).required(),
     alt: Joi.string().min(2).max(200).required(),
   }),
-  phone: Joi.string().min(9).max(15).pattern(phoneRegex).required(),
+  phone: Joi.string()
+    .min(9)
+    .max(15)
+    .pattern(phoneRegex)
+    .messages({
+      "string.empty": "This field is required",
+      "string.min": "Must be a valid Israeli phone number",
+      "string.pattern.base": "Must be a valid Israeli phone number",
+    })
+    .required(),
   email: Joi.string()
     .email({ tlds: { allow: false } })
     .min(5)
     .max(255)
     .required(),
-  password: Joi.string().pattern(passwordRegex).min(9).max(40).required(),
+  password: Joi.string()
+    .pattern(passwordRegex)
+    .min(9)
+    .max(40)
+    .messages({
+      "string.pattern.base":
+        "Password must be at least nine characters long and contain an uppercase letter, a lowercase letter, a number and one of the following characters !@#$%^&*-",
+      "string.empty": "This field is required",
+    })
+    .required(),
   isBusiness: Joi.boolean().required(),
 });
 
